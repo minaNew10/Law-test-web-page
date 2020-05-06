95e6924f2e24bb3071d8db362709e03fec4bd99d
MinaN <minadevdream@gmail.com>
2020-05-06 10:26:55 +0200

    chore : add firebase sdk

--- script.js
+++ script.js
@@ -2,11 +2,12 @@
 document.getElementById("btnSaveBranchedCategory").onclick = function(){
 	
 	var text = document.getElementById("txtBranchedCatName").value
-	alert(" تم حفظ التصنيف الفرعي " + text)
+	
 }
 document.getElementById("btnSaveMainCategory").onclick = function(){
 	var text = document.getElementById("txtMainCatName").value
-	alert(" تم حفظ التصنيف الرئيسي " + text )
+	
+	writeMainCategory(text)
 }
 document.getElementById("btnSaveQuestion").onclick = function(){
 	alert(" تم حفظ السؤال " + displayRadioValue())
@@ -21,4 +22,26 @@ function displayRadioValue() {
 	}
 	return text;
 } 
+	  var firebaseConfig = {
+		apiKey: "AIzaSyBMreciTLPtbvTuCKiSBt48LVDOouQVwLg",
+		authDomain: "law-test-7a1ff.firebaseapp.com",
+		databaseURL: "https://law-test-7a1ff.firebaseio.com",
+		projectId: "law-test-7a1ff",
+		storageBucket: "law-test-7a1ff.appspot.com",
+		messagingSenderId: "215803553745",
+		appId: "1:215803553745:web:d6f6caf93408b7209d22f0"
+	  };
+	  
+	  firebase.initializeApp(firebaseConfig);
+	  firebase.database().ref("categories").once("value").then(function(snapshot) {
+		document.getElementById("object").innerHTML = JSON.stringify(snapshot.val(), null, 2); 
+	});
+	
+	
+function writeMainCategory(category){
+	alert(" تم حفظ التصنيف الرئيسي " + category )
+	firebase.database().ref("categories").push({
+		name : category
+	})
+}
 
