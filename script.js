e262f86c5b01e2153afbd70599cee0d5c920eb82
MinaN <minadevdream@gmail.com>
2020-05-12 20:35:06 +0200

    fix: the bug of null category

--- script.js
+++ script.js
@@ -1,93 +1,105 @@
 
+
+getMainCategories()
 document.getElementById("btnSaveMainCategory").onclick = function(){
 	var text = document.getElementById("txtMainCatName").value
-	console.log(text)
+	
 	writeMainCategory(text)
 }
 document.getElementById("btnSaveBranchedCategory").onclick = function(){
 	
 	var branchedCat = document.getElementById("txtBranchedCatName").value
-	var select = document.getElementById("selectCat0")
-    
-	var index = select.selectedIndex
-	var optionsItems = select.options
-	var mainCat = optionsItems[index].text
-	
-	document.getElementById("object").innerHTML = mainCat; 
+	var select = document.getElementById("selectCat0")    
+	mainCat = getSelectValue(select)	
 	writeBranchedCategory(branchedCat,mainCat)
 	
 }
 document.getElementById("btnSaveQuestion").onclick = function(){
-	var select = document.getElementById("selectCat1")
+	/*var select = document.getElementById("selectCat1")
 	var index = select.selectedIndex
 	var optionsItems = select.options
 	var mainCat = optionsItems[index].text
+	question = createQuestion()
 	
-	alert(" تم حفظ السؤال " )
+	writeQuestion(question)
+	alert(" تم حفظ السؤال " )*/
+	var selectMain = document.getElementById("selectCat1")
+	var selectbranched = document.getElementById("branchedCat")
+	
+	var mainClassification = getSelectValue(selectMain)
+	
+	
+	var branchedClassification = getSelectValue(selectBranched)
+	
+	alert(mainClassification + " " + branchedClassification)
+	
 }
 
-function getRadioValue() { 
-	var ele = document.getElementsByName("difficulty"); 
-	var text 
-	for(i = 0; i < ele.length; i++) { 
-		if(ele[i].checked) 
-			text = ele[i].value; 
+function createQuestion(){
+	var questionBody = document.getElementById("question")
+	var choice1 = document.getElementById("txtFirstChoice").value
+	var choice2 = document.getElementById("txtSecChoice").value
+	var choice3 = document.getElementById("txtThirdChoice").value
+	var choice4 = document.getElementById("txtForChoice").value
+	var choices = {
+		1 : choice1,
+		2 : choice2,
+		3 : choice3,
+		4 : choice4,
 	}
-	return text;
-} 
-	  intializeFirebase()
-	  getMainCategories()
-	  getBranchedCategories("دولي")
-	  
 
+	var correctAnswer = document.getElementById("txtCorrectAns").value
+	var ansRef = document.getElementById("ansReference").value
+	var difficulty = getRadioValue()
+	var selectMain = document.getElementById("selectCat1")
+	var selectbranched = document.getElementById("branchedCat")
+	var mainClassification = getSelectValue(selectMain)
+	
+	var branchedClassification = getSelectValue(selectBranched)
+	
+	var classification  = getSelectValue(selectBranched)+ " " + getSelectValue(selectMain)
+	classification = classification.replace(/\s/g, "_");
+	var question = {
+		questionBody : questionBody,
+		choices : choices,
+		correctAnswer : correctAnswer,
+		ansRef : ansRef,
+		difficulty : difficulty,
+		classification : classification
+	}
+	return question
+}
+function writeQuestion(question){
+	firebase.database().ref("questions/" + question.classification).set({
+		ques : question
+	})
+}
+ 
 function getMainCategories(){
   firebase.database().ref("categories").once("value").then(function(snapshot) {
-  categories = makeCategoryList(snapshot)
-  console.log("Categories " + categories) 
-  createMainCatSelectOptions(categories)
-  
+	  categories = makeCategoryList(snapshot)
+	  createMainCatSelectOptions(categories)
   }); 
  
 }
+function makeCategoryList(snapshot) {
+  var catKeys = Object.keys(snapshot.val())
+  var catValues = Object.values(snapshot.val())
+  var categories = []
+  var category = {name:"",key:""}
+  for(i = 0; i < catValues.length; i++) { 
+	category = catValues[i]
+	categories.push(category)
+ } 
+ return categories 
+}
 function getBranchedCategories(mainCat){
 	firebase.database().ref("categories/" + mainCat).once("value").then(function(snapshot) {
-    
 	branchedCategories = makeCategoryList(snapshot)
 	createBrancedCatSelectOptions(branchedCategories)
   
   }); 
 }
-	  
-		/*
-		var arrKeys = Object.keys(snapshot.val())
-		var category = {name:"",key:"",branches : {}}
-		var arrValues = Object.values(snapshot.val())
-		category = arrValues[0]
-		category.key = arrKeys[0]
-		//document.getElementById("object").innerHTML = JSON.stringify(category.name + category.key, null, 2); 
-		 snapshot.forEach(function(childSnapshot){
-			   var childKey = childSnapshot.key;
-			   var data = childSnapshot.val();
-		   //  document.getElementById("object").innerHTML = JSON.stringify(snapshot.val(), null, 2); 
-		    // document.getElementById("object").innerHTML = JSON.stringify(childSnapshot.val(), null, 2); 
-		     document.getElementById("object").innerHTML = JSON.stringify(childKey, null, 2); 
-		  });
-		  */
-
-	
-function intializeFirebase(){
-	 var firebaseConfig = {
-		apiKey: "AIzaSyBMreciTLPtbvTuCKiSBt48LVDOouQVwLg",
-		authDomain: "law-test-7a1ff.firebaseapp.com",
-		databaseURL: "https://law-test-7a1ff.firebaseio.com",
-		projectId: "law-test-7a1ff",
-		storageBucket: "law-test-7a1ff.appspot.com",
-		messagingSenderId: "215803553745",
-		appId: "1:215803553745:web:d6f6caf93408b7209d22f0"
-	  };
-	  
-	  firebase.initializeApp(firebaseConfig);
-}	
 function writeMainCategory(category){
 	alert(" تم حفظ التصنيف الرئيسي " + category )
 	firebase.database().ref("categories/" + category).set({
@@ -100,29 +112,31 @@ function writeBranchedCategory(branchedCat,mainCat){
 		name : branchedCat
 	})
 }
-function createQuestion(){
-	var question = {
+
+
+function getRadioValue() { 
+	var ele = document.getElementsByName("difficulty"); 
+	var text 
+	for(i = 0; i < ele.length; i++) { 
+		if(ele[i].checked) 
+			text = ele[i].value; 
+	}
+	
+	return text;
+}
+function getSelectValue(selectElement){
+	var index = selectElement.selectedIndex
+	var optionsItems = selectElement.options
+	var selected = optionsItems[index].text	
+	return selected
+}
+function printArr(arr){
+	for(var i = 0; i < arr.length; i++){
 		
 	}
 }
-function makeCategoryList(snapshot) {
-  var catKeys = Object.keys(snapshot.val())
-  var catValues = Object.values(snapshot.val())
-  var categories = []
-  var category = {name:"",key:""}
-  for(i = 0; i < catValues.length; i++) { 
-	category = catValues[i]
-	categories.push(category)
- } 
-
- return categories 
-}
-function populatDoc(categories){
-	document.getElementById("object").innerHTML = JSON.stringify(categories[0].name, null, 2);
-}
 //pass array of type category
 function createMainCatSelectOptions(arr){
-	
 	
 	var newSelect= document.createElement("select");
 	for(i = 0; i < arr.length; i++){
@@ -130,6 +144,7 @@ function createMainCatSelectOptions(arr){
 	   
 	   opt.value = arr[i].name;
 	   opt.innerHTML = arr[i].name; // whatever property it has
+	  
 	   // then append it to the select element
 	   newSelect.appendChild(opt);
 	}
@@ -156,9 +171,13 @@ function createMainCatSelectOptions(arr){
 }
 
 function createBrancedCatSelectOptions(branchedCategories){
+	
+	
 	branchedBar = document.getElementById("branchedCatSelect")
 	selectBranched = document.getElementById("branchedCat") 
+	
 	if(document.getElementById("branchedCat") != null){
+		
 		branchedBar.removeChild(selectBranched)
 	}
 	//check if it is already present in order not to create it again
@@ -170,16 +189,34 @@ function createBrancedCatSelectOptions(branchedCategories){
 				if(branchedCategories[i].name != null){
 				   var opt = document.createElement("option");	   
 				   opt.value = branchedCategories[i].name;
+				   
 				   opt.innerHTML = branchedCategories[i].name; // whatever property it has
 				   // then append it to the select element
-				   
+				  
 				   newSelect.appendChild(opt);
 				}
 			}
 		}
 		newSelect.setAttribute("id","branchedCat")
 		branchedBar.appendChild(newSelect)
+	selectBranched = document.getElementById("branchedCat") 
+	
 	
 }
 
 
+	/*
+		var arrKeys = Object.keys(snapshot.val())
+		var category = {name:"",key:"",branches : {}}
+		var arrValues = Object.values(snapshot.val())
+		category = arrValues[0]
+		category.key = arrKeys[0]
+		//document.getElementById("object").innerHTML = JSON.stringify(category.name + category.key, null, 2); 
+		 snapshot.forEach(function(childSnapshot){
+			   var childKey = childSnapshot.key;
+			   var data = childSnapshot.val();
+		   //  document.getElementById("object").innerHTML = JSON.stringify(snapshot.val(), null, 2); 
+		    // document.getElementById("object").innerHTML = JSON.stringify(childSnapshot.val(), null, 2); 
+		     document.getElementById("object").innerHTML = JSON.stringify(childKey, null, 2); 
+		  });
+		  */
